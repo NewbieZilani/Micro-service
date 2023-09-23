2b2689bea467820000ea8d951484ec328ea75630
Zilani Mia <141901820+NewbieZilani@users.noreply.github.com>
2023-09-23 21:54:45 +0600

    Delete

--- HotelService/src/main/java/com/microservice/main/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.main.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- HotelService/src/main/java/com/microservice/main/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.main.repository;
-
-import com.microservice.main.entity.HotelEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<HotelEntity, String> {
-
-}
