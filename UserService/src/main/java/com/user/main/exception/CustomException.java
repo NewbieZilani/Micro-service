f2b5bfc5b150743f14001dd59b7b5d9eaa101f29
Zilani Mia <141901820+NewbieZilani@users.noreply.github.com>
2023-09-23 21:55:31 +0600

    Delete

--- UserService/src/main/java/com/user/main/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.user.main.exception;
-
-public class CustomException extends RuntimeException{
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- UserService/src/main/java/com/user/main/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.user.main.repository;
-
-import com.user.main.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-
-}
